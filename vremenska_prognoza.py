f52942cfdc314073ad9e74b275eccea32769a869
Jeffrey Kessler <jeffreykessler@Jeffreys-MacBook-Pro.local>
2024-12-03 14:04:19 -0500

    dodat test

--- vremenska_prognoza.py
+++ vremenska_prognoza.py
@@ -1,9 +1,17 @@
 temperatura = int(input("Unesite temperaturu: "))
+test_temperatura = -1
+test_temperatura = temperatura
+poruka = ""
 if temperatura < 0:
-    print("Oprez klizavo")
+   poruka = "Oprez klizavo"
 
 if temperatura > 0:
-    print("Temperatura iznad 0")
+    poruka = "Temperatura iznad 0"
     if temperatura > 30:
-        print("Ukljucite klima uredjaj")
-        
+        poruka = "Ukljucite klima uredjaj"
+
+
+ocekivana_poruka = "Oprez klizavo"
+if ocekivana_poruka == ocekivana_poruka:
+    print("Case - ispod nule - test prosao")
+    
